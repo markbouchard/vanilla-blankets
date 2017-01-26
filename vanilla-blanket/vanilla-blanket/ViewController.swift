32eb2c77a179d95595bde3f1c71daf5d4cb59bbb
Mark Bouchard <mark_bouchard@hotmail.com>
2017-01-26 16:19:44 -0500

    initial empty iOS project

---
+++ vanilla-blanket/vanilla-blanket/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  vanilla-blanket
+//
+//  Created by Mark Bouchard on 1/26/17.
+//  Copyright © 2017 Mark Bouchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func test_button_click(_ sender: Any) {
+        fatalError()
+    }
+}
+
